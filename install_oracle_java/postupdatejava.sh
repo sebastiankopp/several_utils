d135164cf908ff841fce81d8d2c1dd9b95c65436
sebi <sebi@hypertux>
2017-10-23 12:07:57 +0200

    First commit, added shellscript

---
+++ install_oracle_java/postupdatejava.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for i in  jar jarsigner java javac javadoc jaotc javapackager javaws jcmd jconsole jcontrol jdb jdeprscan jdeps jhsdb jimage jinfo jjs jlink jmap jmc jmod jps jrunscript jshell jstack jstat jstatd jweblauncher keytool pack200 policytool schemagen unpack200 wsgen wsimport xjc
+do
+	if [ -f $JAVA_HOME/bin/$i ]
+	then
+		update-alternatives --install /usr/bin/$i $i $JAVA_HOME/bin/$i 1
+		update-alternatives --set $i $JAVA_HOME/bin/$i
+	else
+		>&2 echo "Die Datei $JAVA_HOME/bin/$i existiert nicht!"
+	fi
+done
+
