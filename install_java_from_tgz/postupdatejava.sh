a0fb7d708af0deddd53e12664839b36e7757e03a
sebi <sebi@hypertux>
2017-11-25 13:12:41 +0100

    Corrected some issues

--- install_java_from_tgz/postupdatejava.sh
+++ install_java_from_tgz/postupdatejava.sh
@@ -6,8 +6,10 @@ do
 	then
 		update-alternatives --install /usr/bin/$i $i $JAVA_HOME/bin/$i 1
 		update-alternatives --set $i $JAVA_HOME/bin/$i
+	#	sudo update-alternatives --install /usr/bin/$i $i $JAVA_HOME/bin/$i 1
+	#	sudo update-alternatives --set $i $JAVA_HOME/bin/$i
 	else
-		>&2 echo "The file $JAVA_HOME/bin/$i does not exist!"
+		>&2 echo "The file $JAVA_HOME/bin/$i does not exist or is not executable!"
 	fi
 done
 
