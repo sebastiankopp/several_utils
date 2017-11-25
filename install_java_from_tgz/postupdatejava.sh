b5d091d6c29688e24356e0816c6e76403e79faf9
sebi <sebi@hypertux>
2017-11-25 13:04:28 +0100

    Integrated check if file in $JAVA_HOME/bin ist executable

--- install_java_from_tgz/postupdatejava.sh
+++ install_java_from_tgz/postupdatejava.sh
@@ -2,7 +2,7 @@
 
 for i in `ls $JAVA_HOME/bin | tr '\n' ' '`
 do
-	if [ -f $JAVA_HOME/bin/$i ]
+	if [ -x $JAVA_HOME/bin/$i ]
 	then
 		update-alternatives --install /usr/bin/$i $i $JAVA_HOME/bin/$i 1
 		update-alternatives --set $i $JAVA_HOME/bin/$i
