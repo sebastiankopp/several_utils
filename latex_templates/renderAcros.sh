47f447529513087824532f291987f2cf6b81d723
Sebastian Kopp <sebastian.kopp93@web.de>
2021-01-27 22:00:37 +0100

    Improved line comments in acronym rendering script

--- latex_templates/renderAcros.sh
+++ latex_templates/renderAcros.sh
@@ -22,11 +22,11 @@ then
 fi
 
 # Compute max width
-widest_abbrev=$(grep '\S\+' "$infile" | grep -v '^#' | awk -F= '{print length($1)" "$1}' | sort -n | tail -n 1 | awk '{print $2}') || exit 44
+widest_abbrev=$(sed -rs 's/#.+$//' "$infile" | grep '\S\+' | awk -F= '{print length($1)" "$1}' | sort -n | tail -n 1 | awk '{print $2}') || exit 44
 
 echo "Widest abbrevation: $widest_abbrev"
 
 echo "\\begin{acronym}[${widest_abbrev}]" > "$outfile" || exit 44
-grep '\S\+' "$infile" | grep -v '^#' | sort | awk -F= '{print "\\acro{"$1"}["$1"]{"$2"}"}' >> "$outfile" || exit 44
+sed -rs 's/#.+$//' "$infile" | grep '\S\+' | sort | awk -F= '{print "\\acro{"$1"}["$1"]{"$2"}"}' >> "$outfile" || exit 44
 echo '\end{acronym}' >> "$outfile" || exit 44
 
